6b16b27ec4963e7d6e9bb5323bd1ad2f6dd8a711
carlos.claros <carlos.claros@dharbor.com>
2021-09-09 09:01:15 -0400

    sexto commit

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -6,6 +6,8 @@ import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.config.RestAssuredConfig.config;
 import static org.hamcrest.Matchers.*;
 
 
@@ -15,48 +17,15 @@ public class SWApiTestWithRestAssured {
     @Test
     public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() {
 
-        BaseApiResponse baseApiResponse = RestAssured
-                .given()
-                .baseUri("https://swapi.dev/api" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "")
-                .and()
-                .queryParam("format", "json")
-                .when()
-                .get("/")
-                .then()
-                .log().all()
-                .and().assertThat().statusCode(is(equalTo(200)))
-                .and()
-                .body("films", response -> notNullValue())
-                .body("vehicles", response -> notNullValue())
-                .body("people", response -> notNullValue())
-                .body("starships", response -> notNullValue())
-                .body("species", response -> notNullValue())
-                .body("planets", response -> notNullValue())
-                .and().extract().body().as(BaseApiResponse.class);
 
         RestAssured
                 .given()
                 .queryParam("format", "json")
-                .log().all()
-                .when()
-                .post(baseApiResponse.getPlanets())
-                .then()
-                .log().all()
-                .and()
-                .assertThat()
-                .statusCode(is(equalTo(405)));
+                .config(config().logConfig(logConfig()
+                        .enableLoggingOfRequestAndResponseIfValidationFails()))
+                .when().get("https://swapi.dev/api/planets/?format=json?")
+                .then().assertThat()
+                .statusCode(is(equalTo(200)));
 
     }
 
