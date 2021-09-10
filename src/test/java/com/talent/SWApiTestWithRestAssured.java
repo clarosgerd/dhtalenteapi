0d55841003cec54bca081e3e5fd338e006f55bcf
carlos.claros <carlos.claros@dharbor.com>
2021-09-10 06:55:32 -0400

    octavo  commit

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -1,41 +1,62 @@
 package com.talent;
 
 
-import com.talent.bind.BaseApiResponse;
+
+import com.talent.log.LoggerOutputStream;
 import io.restassured.RestAssured;
+
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import org.slf4j.Logger;
+
+
+import java.io.IOException;
+import java.io.PrintStream;
 
 import static io.restassured.config.LogConfig.logConfig;
 import static io.restassured.config.RestAssuredConfig.config;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 
 
 
 public class SWApiTestWithRestAssured {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
-    public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() {
+    public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() throws IOException {
 
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        try (LoggerOutputStream outputStream = new LoggerOutputStream(logger)) {
+            RestAssured.config = config().logConfig(logConfig()
+                    .defaultStream(new PrintStream(outputStream))
+                    .enableLoggingOfRequestAndResponseIfValidationFails());
 
-        RestAssured
-                .given()
-                .queryParam("format", "json")
-                .when()
-                .get("https://swapi.dev/api/")
-                .then()
-                .assertThat().statusCode(is(equalTo(200)));
+            RestAssured
+                    .given()
+                    .log().method().and().log().uri().and().log().params()
+                    .queryParam("format", "json")
+                    .when()
+                    .get("https://swapi.dev/api/")
+                    .then()
+                    .assertThat().statusCode(is(equalTo(200)));
 
-        RestAssured
-                .given()
-                .queryParam("format", "json")
-                .when()
-                .post("https://swapi.dev/api/films/")
-                .then()
-                .assertThat().statusCode(is(equalTo(200)));
+            outputStream.flush();
+
+            RestAssured
+                    .given()
+                    .queryParam("format", "json")
+                    .when()
+                    .post("https://swapi.dev/api/films/")
+                    .then()
+                    .assertThat().statusCode(is(equalTo(200)));
+
+            outputStream.flush();
+        }
+
     }
-
 
 
 }
